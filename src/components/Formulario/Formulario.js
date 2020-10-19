3552d7c08119dcf4fe4608ec968ae632c59976ea
LuisDev <lurojas@ucm.es>
2020-10-20 00:45:16 +0200

    Cambios en el diseño
    Materialize

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,9 +1,74 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import "./Formulario.css"
-import Dark from '../DarkTheme/DarkTheme'
-import {addAuthor,deleteAuthor} from '../../js/utils'
+import { deleteAuthor } from '../../js/utils'
+import M from "materialize-css"
+
 
 const Formulario = () => {
+
+    //Array con los nombre de los autores
+    const autores = [];
+
+    //Funcion que se ejecuta cuando se escribe en algun input
+    const guardarAutores = (e) => {
+        e.preventDefault();
+
+        var nAutores = document.querySelectorAll("form .writeAuthor");
+        nAutores.forEach((nombre) => {
+            if (nombre.value !== "")
+                autores.push(nombre.value);
+        });
+
+    };
+
+    const addAuthor = (e) => {
+        //seleccionamos el formulario
+        var formulario = document.querySelector("form > div");
+        //antes de crear el input tenemos que asignarle bien el id de su name
+        var inputs = document.querySelectorAll("form .writeAuthor");
+        var lastChild = inputs[inputs.length - 1];
+        var name = lastChild.getAttribute("name");
+        var number = parseInt(name.split("r")[1]);
+
+        //creamos el input
+        var input = document.createElement("input");
+        input.classList.add("writeAuthor");
+        input.classList.add("validate");
+        input.setAttribute("name", "author" + (number + 1));
+        input.setAttribute("type", "text");
+        //creamos la label
+        var label = document.createElement("label");
+        label.innerHTML = "Nombre del autor:";
+        label.setAttribute("for", "author" + (number + 1));
+
+        //creamos el span que es donde ira el borrar 
+        var span = document.createElement("span");
+        span.innerText = "🗑️";
+        span.classList.add("deleted");
+
+        //cogemos todos los input que hay en la página para saber si se pueden borrar
+        //añadimos la funcionalidad que hace que se borre el autor
+        span.onclick = (e) => {
+            var nElements = document.querySelectorAll("form .writeAuthor").length;
+            if (nElements <= 1)
+                M.toast({ html: 'Operación no permitida', classes: 'rounded' });
+            else {
+                input.remove();
+                span.remove();
+            }
+        }
+        var div = document.createElement("div");
+        div.classList.add("input-field");
+        div.classList.add("col");
+        div.classList.add("s12");
+
+        //se lo insertamos
+        div.appendChild(label);
+        div.appendChild(input);
+        div.appendChild(span);
+        formulario.appendChild(div);
+        e.preventDefault();
+    }
 
     return (
         <Fragment>
@@ -12,23 +77,53 @@ const Formulario = () => {
             </h1>
 
             <div id="contenedor-principal">
-                <form>
-                    <div>
-                        <label className="white">Introduce el nombre del autor:</label>
-                        <input type="text" className="writeAuthor" name ="author1"/>
-                        <span className="deleted" onClick={(e) =>{ deleteAuthor(); e.preventDefault() }}>🗑️</span>
+                <form onSubmit={guardarAutores} className="col s12">
+
+                    <div className="input-field col s12">
+                        <label for="author1">Nombre del autor:</label>
+                        <input type="text" className="writeAuthor validate" name="author1" />
+                        <span className="deleted" onClick={(e) => { deleteAuthor(); e.preventDefault() }}>🗑️</span>
                     </div>
 
-                    <button id="addAuthor" onClick={(e) => { addAuthor(); e.preventDefault() }}>
+
+                    <button className="btn" onClick={addAuthor}>
                         Añadir más autores
                     </button>
-
                     <div>
-                        <button type="submit">Aceptar</button>
+                        <input type="submit" id="send" className="btn" value="Aceptar" />
                     </div>
                 </form>
             </div>
 
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Item Name</th>
+                            <th>Item Price</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>Alvin</td>
+                            <td>Eclair</td>
+                            <td>$0.87</td>
+                        </tr>
+                        <tr>
+                            <td>Alan</td>
+                            <td>Jellybean</td>
+                            <td>$3.76</td>
+                        </tr>
+                        <tr>
+                            <td>Jonathan</td>
+                            <td>Lollipop</td>
+                            <td>$7.00</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </Fragment>
     )
 }
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,53 +1,19 @@
-export function addAuthor() {
-    //seleccionamos el formulario
-    var formulario = document.querySelector("form > div");
-
-    //antes de crear el input haremos una cuenta de los que hay 
-    var nElements = document.querySelectorAll("form .writeAuthor").length;
-
-    //creamos el input y le ponemos la case en css
-    var input = document.createElement("input");
-    input.classList.add("writeAuthor");
-    input.setAttribute("Name", "author" + (nElements + 1));
-    input.setAttribute("placeholder","Introduce el nombre del autor");
-
-    //creamos el span que es donde ira el borrar 
-    var span = document.createElement("span");
-    span.innerText = "🗑️";
-    span.classList.add("deleted");
-
-    //cogemos todos los input que hay en la página para saber si se pueden borrar
-
-    //añadimos la funcionalidad que hace que se borre el autor
-    span.onclick = (e) => {
-        var nElements = document.querySelectorAll("form .writeAuthor").length;
-
-        if (nElements + 1 <= 1)
-            alert("Operación no permitida");
-        else {
-            input.remove();
-            span.remove();
-        }
-        e.preventDefault();
-    }
-    //se lo insertamos
-    formulario.appendChild(input);
-    formulario.appendChild(span);
-}
-
+import M from "materialize-css"
 // esta función es solo para el primer autor
 export const deleteAuthor = () => {
+    
+
     var nElements = document.querySelectorAll("form .writeAuthor").length;
     console.log(nElements);
     //miramos si hay mas de un elemento
     if (nElements < 2)
-        alert("Operación no permitida");
+         M.toast({html: 'Operación no permitida', classes: 'rounded'});
     else {
         //seleccionamos el primer autor y le borramos
         var label = document.querySelector("form  label");
         var input = document.querySelector("form .writeAuthor");
         var span = document.querySelector("form span")
-        label.remove(); 
+        label.remove();
         input.remove();
         span.remove();
     }
